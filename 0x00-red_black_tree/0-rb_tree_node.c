b5f419ecf4d461f194a8253ceb093479544f6d30
evertcolombia <evertescalante@gmail.com>
2021-10-04 20:31:33 -0500

    task 0

---
+++ 0x00-red_black_tree/0-rb_tree_node.c
@@ -0,0 +1,33 @@
+#include "rb_trees.h"
+
+#include <stdio.h>
+
+/**
+ * rb_tree_node - create a node for red black tree
+ * @parent: parentf of the new node
+ * @value: value for the node
+ * @color: color of the node
+ *
+ * Return: node on success. NULL if fails
+ */
+rb_tree_t *rb_tree_node(rb_tree_t *parent, int value, rb_color_t color)
+{
+	rb_tree_t *new = NULL;
+
+
+	new = malloc(sizeof(rb_tree_t));
+	if (new == NULL)
+		return (NULL);
+
+	if (color == BLACK)
+		new->color = RED;
+	else
+		new->color = color;
+
+	new->parent = parent;
+	new->n = value;
+	new->left = NULL;
+	new->right = NULL;
+
+	return (new);
+}
