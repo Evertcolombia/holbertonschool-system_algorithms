245f2d658696c02dec6b014f9aef1c7c21ee9c5e
evertcolombia <evertescalante@gmail.com>
2021-10-05 20:01:36 -0500

    task 1 method to check if colors in a red-black tree are organized correctly

--- 0x00-red_black_tree/1-rb_tree_is_valid.c
+++ 0x00-red_black_tree/1-rb_tree_is_valid.c
@@ -1,5 +1,14 @@
 #include "rb_trees.h"
+#include <stdio.h>
 
+/**
+ * check_bst - check if a tree is binary search tree valid
+ * @tree: node to check
+ * @min: min number to compare
+ * @max: max number to compare
+ *
+ * Return: 1 on success, 0 on error
+ */
 int check_bst(const rb_tree_t *tree, int min, int max)
 {
 	if (tree == NULL)
@@ -14,10 +23,45 @@ int check_bst(const rb_tree_t *tree, int min, int max)
 		check_bst(tree->right, (tree->n + 1), max));
 }
 
-int rb_tree_is_valid(const rb_tree_t *tree)
+int check_tree_colors(const rb_tree_t *tree)
 {
 	if (tree == NULL)
+		return (1);
+
+	if (tree->color != RED && tree->color != BLACK)
 		return (0);
 
-	return (check_bst(tree, INT_MIN, INT_MAX));
+	if (tree->color == RED)
+	{
+		if (tree->left && tree->right)
+		{
+			if (tree->left->color != BLACK)
+				return (0);
+
+			if (tree->right->color != BLACK)
+				return (0);
+		}
+	}
+
+	if ((check_tree_colors(tree->left) != 1) ||
+		(check_tree_colors(tree->right) != 1))
+		return (0);
+
+	return (1);
+}
+
+int rb_tree_is_valid(const rb_tree_t *tree)
+{
+	int val = 0;
+
+	if (tree == NULL)
+		return (0);
+	val = check_bst(tree, INT_MIN, INT_MAX);
+
+	if (tree->color == RED)
+		return (0);
+
+	val = check_tree_colors(tree);
+
+	return (val);
 }
