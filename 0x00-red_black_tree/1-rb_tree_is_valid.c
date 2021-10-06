889a3f8b049ae2e5795240e7412d7c5f364d62c7
evertcolombia <evertescalante@gmail.com>
2021-10-05 20:42:24 -0500

    add method for black nodes and betty

--- 0x00-red_black_tree/1-rb_tree_is_valid.c
+++ 0x00-red_black_tree/1-rb_tree_is_valid.c
@@ -23,6 +23,12 @@ int check_bst(const rb_tree_t *tree, int min, int max)
 		check_bst(tree->right, (tree->n + 1), max));
 }
 
+/**
+ * check_tree_colors - check if color nodes are organized
+ * @tree: node to check
+ *
+ * Return: 1 on success, 0 on error
+ */
 int check_tree_colors(const rb_tree_t *tree)
 {
 	if (tree == NULL)
@@ -50,6 +56,35 @@ int check_tree_colors(const rb_tree_t *tree)
 	return (1);
 }
 
+/**
+ * check_black_number - check number of black trees
+ * @tree: node to check
+ *
+ * Return: 1 on success, 0 on error
+ */
+int check_black_number(const rb_tree_t *tree)
+{
+	int leftBlackNodes = 0;
+	int rightBlackNodes = 0;
+
+	if (tree == NULL)
+		return (1);
+
+	leftBlackNodes = check_black_number(tree->left);
+	rightBlackNodes = check_black_number(tree->right);
+
+	if (leftBlackNodes != rightBlackNodes)
+		return (0);
+
+	return (1);
+}
+
+/**
+ * rb_tree_is_valid - check if a red black tree is valid
+ * @tree: node to check
+ *
+ * Return: 1 on sucess, 0 on error
+ */
 int rb_tree_is_valid(const rb_tree_t *tree)
 {
 	int val = 0;
@@ -62,6 +97,6 @@ int rb_tree_is_valid(const rb_tree_t *tree)
 		return (0);
 
 	val = check_tree_colors(tree);
-
+	val = check_black_number(tree);
 	return (val);
 }
